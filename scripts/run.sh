b6bbeb9aead11f47f20d6e86652e963ce4fd2534
jdepons <jdepons@mcw.edu>
2024-04-25 13:09:21 -0500

    upge

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 # Set the JAVA_HOME environment variable to the path of your Java installation
 
 # Run the Java class
-java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator
+java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator
