40b06832b8447e2a906e85ce19bec64fc4624ffe
jdepons <jdepons@mcw.edu>
2024-04-25 12:12:18 -0500

    updated

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Set the JAVA_HOME environment variable to the path of your Java installation
+export CLASSPATH=../libs/text-mining-tools.jar
+
+# Run the Java class
+java edu.mcw.rgd.nlp.PubMedBertAnnotator
