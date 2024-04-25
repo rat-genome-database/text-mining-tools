53515ad85ff1cc45561503ba8886c2cf034d609d
jdepons <jdepons@mcw.edu>
2024-04-25 13:07:12 -0500

    updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
+
 #!/bin/bash
 # Set the JAVA_HOME environment variable to the path of your Java installation
-export CLASSPATH=../libs/text-mining-tools.jar
 
 # Run the Java class
-java edu.mcw.rgd.nlp.PubMedBertAnnotator
+java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator
