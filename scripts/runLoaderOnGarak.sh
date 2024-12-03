29cd991cda2dac897a923064450d525af603cc71
jdepons <jdepons@mcw.edu>
2024-12-03 15:16:31 -0600

    pudated

---
+++ scripts/runLoaderOnGarak.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+# Set the JAVA_HOME environment variable to the path of your Java installation
+
+# Run the Java class
+#java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
+# Run the Java class
+./runPubmedBertLoader.sh "/rgd/pubmed/2024" 1
---
+++ scripts/runPubmedToPMCMappingLoader.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+# Set the JAVA_HOME environment variable to the path of your Java installation
+
+# Run the Java class
+#java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
+# Run the Java class
+java -classpath $1/text-mining-tools/lib/*:$1/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedToPMCMappingLoader "$1"
