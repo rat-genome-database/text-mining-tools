55ef195dc4ccf0b3692c96f3d16e198544c5b4aa
jdepons <jdepons@mcw.edu>
2024-12-04 15:21:57 -0600

    updated

--- src/main/dist/runPMCMapperOnGarak.sh
+++ src/main/dist/runPMCMapperOnGarak.sh
@@ -5,4 +5,4 @@
 # Run the Java class
 #java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
 # Run the Java class
-./runPubmedToPMCMappingLoader.sh "/Users/jdepons/assist/PMC-ids.csv"
+./runPubmedToPMCMappingLoader.sh "/rgd/pubmed/PMC-ids.csv"
