d81e798df0ef6111db59f193daa33bfd9690e596
jdepons <jdepons@mcw.edu>
2024-12-10 15:43:34 -0600

    pudated

---
+++ src/main/dist/runPMCFullTextLoader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Set the JAVA_HOME environment variable to the path of your Java installation
+
+# Run the Java class
+#java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
+# Run the Java class
+/usr/lib/jvm/java-17/bin/java -Dspring.config=/home/rgdpub/properties/default_db2.xml -classpath /home/rgdpub/pipelines/text-mining-tools/lib/*:/home/rgdpub/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PMCFullTextLoader "$1"
