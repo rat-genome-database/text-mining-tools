da2ada60c78c75a3855c021129b7e003fbedfec9
jdepons <jdepons@mcw.edu>
2024-12-12 12:02:22 -0600

    updated

--- src/main/dist/runPubmedBertDBAnnotator.sh
+++ src/main/dist/runPubmedBertDBAnnotator.sh
@@ -5,4 +5,4 @@
 # Run the Java class
 #java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
 # Run the Java class
-java -classpath $1/text-mining-tools/lib/*:$1/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedBertDBAnnotator "$2" "$3" $4 "$5" "$6"
+/usr/lib/jvm/java-17/bin/java -Dspring.config=/home/rgdpub/properties/default_db2.xml -classpath /home/rgdpub/pipelines/text-mining-tools/lib/*:/home/rgdpub/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedBertDBAnnotator "$2" "$3" $4 "$5" "$6"
