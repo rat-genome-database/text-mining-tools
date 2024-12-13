051a5f0bba8fb685945e274b24cf46d180bed882
jdepons <jdepons@mcw.edu>
2024-12-13 15:15:25 -0600

    updated

--- src/main/dist/runAILoader.sh
+++ src/main/dist/runAILoader.sh
@@ -5,4 +5,4 @@
 # Run the Java class
 #java -classpath /home/rgddata/pipelines/text-mining-tools/build/libs/text-mining-tools.jar:/home/rgddata/pipelines/text-mining-tools/build/libs/dev-pubmedbert-textminingtools.jar edu.mcw.rgd.nlp.PubMedBertAnnotator "/data/ai"
 # Run the Java class
-/usr/lib/jvm/java-17/bin/java -Dspring.config=/home/rgdpub/properties/default_db2.xml -classpath /home/rgdpub/pipelines/text-mining-tools/lib/*:/home/rgdpub/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.PubMedBertLoader "/data/ai" "curatorModel:latest"
+/usr/lib/jvm/java-17/bin/java -Dspring.config=/home/rgdpub/properties/default_db2.xml -classpath /home/rgdpub/pipelines/text-mining-tools/lib/*:/home/rgdpub/pipelines/text-mining-tools/build/libs/text-mining-tools.jar edu.mcw.rgd.nlp.AILoader "/data/ai" "curatorModel:latest"
