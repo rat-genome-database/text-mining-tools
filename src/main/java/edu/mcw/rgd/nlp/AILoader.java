883678a51480e78f9579bbcf9b2da7bdf12f650e
jdepons <jdepons@mcw.edu>
2024-12-17 15:52:20 -0600

    pduated

--- src/main/java/edu/mcw/rgd/nlp/AILoader.java
+++ src/main/java/edu/mcw/rgd/nlp/AILoader.java
@@ -5,15 +5,9 @@ import edu.mcw.rgd.dao.impl.GeneDAO;
 import edu.mcw.rgd.nlp.datamodel.ResearchArticle;
 import edu.mcw.rgd.nlp.utils.ncbi.PubMedJSoupDoc;
 import edu.mcw.rgd.nlp.utils.python.HugRunner;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
